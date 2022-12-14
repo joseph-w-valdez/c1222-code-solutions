80d59ba44315179f3491f76f1c13e20cab96adba
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2022-12-14 18:08:59 +0000

    completed javascript-if exercise

---
+++ javascript-if/main.js
@@ -0,0 +1,70 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+function isUnderFive(number) {
+  return number < 5;
+}
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+function startsWithJ(string) {
+  return string.split('')[0] === 'J';
+}
+
+var people = {
+  bart: {
+    name: 'Bart Simpson',
+    age: 10
+  },
+
+  biden: {
+    name: 'Joe Biden',
+    age: 98472347892374
+  }
+};
+
+function isOldEnoughToDrink(person) {
+  return people[person].age >= 21;
+}
+
+function isOldEnoughToDrive(person) {
+  return person.age >= 16;
+}
+
+function isOldEnoughToDrinkAndDrive() {
+  return false;
+}
+
+function categorizeAcidity(pH) {
+  if (pH > 7 && pH <= 14) {
+    return 'basic';
+  }
+
+  if (pH === 7) {
+    return 'neutral';
+  }
+
+  if (pH < 7 && pH >= 0) {
+    return 'acidic';
+  } else {
+    return 'invalid pH level';
+  }
+}
+
+function introduceWarnerBro(name) {
+  if (name === 'yakko' || name === 'wakko') {
+    return "We're the Warner brothers!";
+  } else if (name === 'dot') {
+    return "I'm cute~";
+  } else {
+    return 'Goodnight everybody!';
+  }
+}
