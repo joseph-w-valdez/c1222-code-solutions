2e674136b1fbf1e6883e6644d40906c5628fc695
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2022-12-16 21:59:56 +0000

    WIP testing commit

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,20 @@
+/* exported head */
+
+/*
+pseudocode logic
+----------------
+head(array)
+
+objective
+---------------
+extract the first value of the array and return it
+
+plan
+--------------
+set a variable equal to array[0] and return it
+
+*/
+
+function head(array) {
+  return array[0];
+}
