a7571e0411babaa6c0373f8ff38b35e1d9353df2
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2022-12-18 01:47:44 +0000

    WIP. fixed includes-sevens function error. fixing get-student-names function. switching to desktop

--- javascript-loops-practice/get-student-names.js
+++ javascript-loops-practice/get-student-names.js
@@ -2,7 +2,7 @@
 
 function getStudentNames(students) {
 
-  var result = [];
+  /*   var result = [];
   for (let i = 0; i < students.length; i++) {
     for (const prop in students[i]) {
       result.push(students[i][prop]);
@@ -11,4 +11,30 @@ function getStudentNames(students) {
   return result;
 }
 
-// ['Shrek', 'Donkey', 'Fiona']
+*/
+  /*
+  var students = [
+    {
+      name: 'Shrek'
+    },
+    {
+      name: 'Donkey'
+    },
+    {
+      name: 'Fiona'
+    }
+  ];
+
+  function getStudentNames(students) {
+    var result = [];
+    for (var values in students) {
+      result.push(students[values]);
+    }
+    return result;
+  }
+  // ['Shrek', 'Donkey', 'Fiona']
+*/
+
+/*   console.log(getStudentNames(students));
+// ['Shrek', 'Donkey', 'Fiona'] */
+}
--- javascript-loops-practice/includes-seven.js
+++ javascript-loops-practice/includes-seven.js
@@ -1,12 +1,25 @@
 /* exported includesSeven */
 function includesSeven(array) {
   var result;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 7) {
-      result = true;
-    } else {
-      result = false;
+  if (array.length > 0) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === 7) {
+        return true;
+      } else {
+        result = false;
+      }
     }
+  } else {
+    return false;
   }
+
   return result;
 }
+/*
+console.log('includesSeven([1, 2, 3, 4, 5, 6, 7]): ', includesSeven([1, 2, 3, 4, 5, 6, 7]));
+console.log('includesSeven([77, 200, 700, "7"]): ', includesSeven([77, 200, 700, '7']));
+console.log('includesSeven([1, 2, 3, 4, 5, 6, 7]): ', includesSeven([1, 2, 3, 4, 5, 6, 7]));
+console.log('includesSeven([234, "7", 7, 322, 4]): ', includesSeven([234, '7', 7, 322, 4]));
+console.log('includesSeven([]): ', includesSeven([]));
+console.log('includesSeven([11,7,34,55,43]): ', includesSeven([11, 7, 34, 55, 43]));
+ */
