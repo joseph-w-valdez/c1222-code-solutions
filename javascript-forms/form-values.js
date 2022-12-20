1a098aa6b744c5dfc82a2dfe67c4251913541d6b
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2022-12-20 03:38:34 +0000

    WIP. switching to desktop

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,8 @@
+/* var $submit = document.querySelector('button');
+$submit.addEventListener('submit', handleSubmission);
+
+function handleSubmission(event) {
+  this.event.preventDefault();
+
+}
+ */
