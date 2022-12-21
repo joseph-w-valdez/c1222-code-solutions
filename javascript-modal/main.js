ee6b9dddf819b3945b5a45e9d28ea8573f828de1
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2022-12-21 19:59:40 +0000

    WIP. just need to fix styling. functionality intact.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,5 +1,13 @@
-var $buttonOpenModal = document.querySelector('form');
-function handleOpenModal(event) {
+var $buttonOpenModal = document.querySelector('.form-open');
+var $buttonCloseModal = document.querySelector('.form-popup');
+var $popup = document.querySelector('.overlay');
+function handleModal(event) {
   event.preventDefault();
+  if (event.target.className === 'form-open') {
+    $popup.classList.remove('hidden');
+  } else if (event.target.className === 'form-popup') {
+    $popup.classList.add('hidden');
+  }
 }
-$buttonOpenModal.addEventListener('submit', handleOpenModal);
+$buttonOpenModal.addEventListener('submit', handleModal);
+$buttonCloseModal.addEventListener('submit', handleModal);
