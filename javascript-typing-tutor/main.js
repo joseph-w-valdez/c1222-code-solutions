d85ed30d745aaf305492104b064dde53686f303d
Joseph Valdez <josepg.w.valdez@gmail.com>
2022-12-23 02:24:31 +0000

    WIP. finished accuracy display. need to add replay button.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -24,11 +24,19 @@ function addLetter(letterNumber) {
   var $newCharacter = document.createElement('span');
   return $newCharacter;
 }
+
+var $successes = 0;
+var $fails = 0;
+var $successRate;
+var $successRateRounding;
+var $accuracy = document.querySelector('.accuracy');
+$accuracy.textContent = '0';
 var $phraseNumber = 0;
 var $phraseString = phrases[$phraseNumber];
 var $activeCharacterIndex = 0;
 var $firstRow = document.querySelector('.row');
 var $newCol;
+var finished;
 displayPhrase($phraseNumber);
 
 function displayPhrase(phraseNumber) {
@@ -57,12 +65,23 @@ function checkIfException(character) {
 function nextCharacterIndex() {
   if ($activeCharacterIndex === $phraseString.length - 1) {
     $newCol.remove();
-    $phraseNumber++;
-    $activeCharacterIndex = 0;
-    displayPhrase($phraseNumber);
-    $allSpans = document.querySelectorAll('span');
-    $activeSpan = $allSpans[$activeCharacterIndex];
-    $activeSpan.classList.add('active-letter');
+    if ($phraseNumber === phrases.length - 1) {
+      $newCol = document.createElement('div');
+      $newCol.setAttribute('class', 'col-100');
+      var $congratulations = document.createElement('span');
+      $congratulations.textContent = "Congratulations! You've beaten the typing test!";
+      $newCol.appendChild($congratulations);
+      $firstRow.appendChild($newCol);
+      finished = true;
+
+    } else {
+      $phraseNumber++;
+      $activeCharacterIndex = 0;
+      displayPhrase($phraseNumber);
+      $allSpans = document.querySelectorAll('span');
+      $activeSpan = $allSpans[$activeCharacterIndex];
+      $activeSpan.classList.add('active-letter');
+    }
   } else {
     $activeCharacterIndex++;
     $activeSpan = $allSpans[$activeCharacterIndex];
@@ -74,17 +93,27 @@ var $activeSpan = $allSpans[$activeCharacterIndex];
 $activeSpan.classList.add('active-letter');
 
 function handleKeyPress(event) {
+  var $successfulPresses = document.querySelector('.successes');
+  var $failedPresses = document.querySelector('.fails');
 
-  if (!checkIfException(event.key)) {
+  if (!checkIfException(event.key) && !finished) {
     if (event.key === $activeSpan.textContent) {
       $activeSpan.classList.add('successful-press');
       $activeSpan.classList.remove('failed-press');
       $activeSpan.classList.remove('active-letter');
-
+      $successes++;
       nextCharacterIndex();
-
+      $successfulPresses.textContent = $successes;
+      $successRate = 100 * $successes / ($successes + $fails);
+      $successRateRounding = Math.round($successRate * 100) / 100;
+      $accuracy.textContent = $successRateRounding;
     } else {
       $activeSpan.classList.add('failed-press');
+      $fails++;
+      $failedPresses.textContent = $fails;
+      $successRate = 100 * ($successes + 0) / ($successes + $fails);
+      $successRateRounding = Math.round($successRate * 100) / 100;
+      $accuracy.textContent = $successRateRounding;
     }
   } else {
     return undefined;
